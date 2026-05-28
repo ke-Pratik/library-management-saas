db81e1bd144e678a41280b2c311da985770045ff
ke-Pratik <94440385+ke-Pratik@users.noreply.github.com>
2026-05-28 14:15:02 +0530

    Update Student.java

--- backend/src/main/java/com/studycenter/entity/Student.java
+++ backend/src/main/java/com/studycenter/entity/Student.java
@@ -8,7 +8,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
